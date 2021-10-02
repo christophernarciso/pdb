2699ded16c49040da3fec73a42a8843287b83888
Kurisu <Divinityforvalor@gmail.com>
2021-10-01 21:21:59 -0400

    Small friday changes

--- js/init.js
+++ js/init.js
@@ -9,3 +9,20 @@ document.addEventListener('DOMContentLoaded', () =>
       footerDate[0].innerHTML = new Date().getFullYear();
     }
   }));
+
+  let scrollpos = window.scrollY;
+  const imgs = document.querySelectorAll(".simg");
+
+  window.addEventListener('scroll', function() { 
+    scrollpos = window.scrollY;
+
+    imgs.forEach(i => {
+      if (scrollpos >= 350) {
+        i.classList.remove('fade-out');
+        i.classList.add('fade-in');
+      } else {
+        i.classList.remove('fade-in');
+        i.classList.add('fade-out');
+      }
+    });
+  })
